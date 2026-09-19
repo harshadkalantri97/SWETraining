09f9f260cd06bdaae2b1aac3e2adef6f80fad79b
Harshad Kalantri <hkalantri@mycom-internal.com>
2026-09-20 03:14:34 +0530

    New gitignore file

--- src/dsa/linkedList/DeleteLL.java
+++ src/dsa/linkedList/DeleteLL.java
@@ -1,5 +1,7 @@
 package dsa.linkedList;
 
+import java.util.Scanner;
+
 class ListNode1
 {
    int data;
@@ -26,16 +28,16 @@ class ListNode1
 
 public class DeleteLL
 {
+   // Removing first element in LinkedList
    public static ListNode1 deleteNode(ListNode1 head)
    {
       if (head == null)
          return null;
-      ListNode1 temp = head;
       head = head.next;
-      temp = null;
       return head;
    }
 
+   // Removing last Element in Linkedlist
    public static ListNode1 removeTail(ListNode1 head)
    {
       if (head == null || head.next == null)
@@ -49,17 +51,28 @@ public class DeleteLL
       return head;
    }
 
-   public static ListNode1 arrayToLinkedList(int[] arr)
+   // Removing kth element from LinkedList
+   public static ListNode1 removeK(ListNode1 head, int k)
    {
-      if (arr.length == 0)
-         return null;
-      ListNode1 head = new ListNode1(arr[0]);
-      ListNode1 current = head;
-      for (int i = 1; i < arr.length; i++)
+      if (head == null || k <= 0)
+         return head;
+      if (k == 1)
+         return head.next;
+      int counter = 0;
+      ListNode1 temp = head;
+      ListNode1 prev = null;
+      while (temp != null)
       {
-         current.next = new ListNode1(arr[i]);
-         current = current.next;
+         counter++;
+         if (counter == k)
+         {
+            prev.next = temp.next;
+            return head;
+         }
+         prev = temp;
+         temp = temp.next;
       }
+      System.out.println("The kth value is more than the length of the LinkedList.");
       return head;
    }
 
@@ -75,19 +88,42 @@ public class DeleteLL
       System.out.println("null");
    }
 
+   // Converting array to LinkedList
+   public static ListNode1 arrayToLinkedList(int[] arr)
+   {
+      if (arr.length == 0)
+         return null;
+      ListNode1 head = new ListNode1(arr[0]);
+      ListNode1 current = head;
+      for (int i = 1; i < arr.length; i++)
+      {
+         current.next = new ListNode1(arr[i]);
+         current = current.next;
+      }
+      return head;
+   }
+
    public static void main(String[] args)
    {
       int[] arr = { 1, 3, 5, 7, 9, 11 };
+
       ListNode1 newNode = arrayToLinkedList(arr);
-      ListNode1 newTailNode = newNode;
+      ListNode1 newTailNode = arrayToLinkedList(arr);
+      ListNode1 kthNode = arrayToLinkedList(arr);
       System.out.println("Original List:");
       printLinkedList(newNode);
-      System.out.println("Delete head node list: ");
+      System.out.println("Delete head node list:");
       newNode = deleteNode(newNode);
       printLinkedList(newNode);
-      System.out.println("Delete Tail: ");
+      System.out.println("Delete Tail:");
       newTailNode = removeTail(newTailNode);
       printLinkedList(newTailNode);
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the kth Element");
+      kthNode = removeK(kthNode, sc.nextInt());
+      System.out.println("kthNodeRemoval:");
+      printLinkedList(kthNode);
+      sc.close();
    }
 
 }
